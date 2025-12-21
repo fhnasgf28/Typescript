6e3ec2081d587ff6233119f692afc9ab9b6b6a5b
Farhan Assegaf <82705668+fhnasgf28@users.noreply.github.com>
2025-12-21 07:48:34 +0700

    latihan interface

--- belajar-ts/gemini/latihan_ts2.ts
+++ belajar-ts/gemini/latihan_ts2.ts
@@ -3,3 +3,12 @@ enum Category {
     Furniture  = "bangku_kerja",
     OfficeSupplies = "OFFICE_SUPPLIES"
 }
+
+interface Product {
+    readonly id: number;
+    name: string;
+    category: Category;
+    stock: number;
+    description?: string;
+}
+
